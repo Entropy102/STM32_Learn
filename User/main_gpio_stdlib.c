744a625b53218c5c672d13b5d17820de55ad29c6
Entropy102 <1643933367@qq.com>
2026-08-25 18:09:01 +0800

    完善主函数

--- User/main_gpio_stdlib.c
+++ User/main_gpio_stdlib.c
@@ -2,5 +2,20 @@
 
 int main(void)
 {
-	//RCC_APB2PeriphClockCmd(
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	//初始化PA8端口
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	//将PA8端口设置为高电平，低电平使用ResetBits
+	//GPIO_SetBits(GPIOA,GPIO_Pin_8);
+	GPIO_ResetBits(GPIOA,GPIO_Pin_8);
+	
+	while(1)
+	{
+		
+	}
 }
